6ad2cd3720120b468da84b70fbba1f9e3d5453da
Eldah0 <ehysa960@gmail.com>
2023-05-25 18:14:16 +0200

    first commit

---
+++ JS, Hw/AS2.js
@@ -0,0 +1,5 @@
+var count=0 ;
+function increase(){
+    count++;
+    likes.innerText=count+"like(s)";
+}
---
+++ JS, Hw/As1.js
@@ -0,0 +1,18 @@
+
+function logOut (element){
+
+    if(element.innerText=="LogIn"){
+        element.innerText="logOut";
+    }
+    else{
+        element.innerText="LogIn";
+    }
+
+}
+
+function removeButton(element){
+    element.remove();
+}
+
+
+
